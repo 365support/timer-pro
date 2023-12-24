5bc70074640972d47d757b17c33fbd9bf93a59a0
365support <365support@naver.com>
2023-12-24 22:19:23 +0900

    feat: pwa setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const withPlugins = require("next-compose-plugins");
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
 
-module.exports = withVanillaExtract(nextConfig);
+const withPWA = require("next-pwa")({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPlugins([withVanillaExtract, withPWA], nextConfig);
