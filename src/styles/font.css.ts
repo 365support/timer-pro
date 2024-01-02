af79dcd35431a69c3e618e637059e8543ca32ac8
365support <365support@naver.com>
2024-01-02 19:17:05 +0900

    refactor: font 최적화

---
+++ src/styles/font.css.ts
@@ -0,0 +1,47 @@
+import { fontFace } from "@vanilla-extract/css";
+
+export const Pretendard = fontFace([
+  {
+    src: [
+      `url("/fonts/Pretendard-Regular.woff") format("woff")`,
+      `url("/fonts/Pretendard-Regular.woff2") format("woff2")`,
+      `local("Pretendard-Regular")`,
+    ],
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontDisplay: "swap",
+  },
+  {
+    src: [
+      `url("/fonts/Pretendard-Medium.woff") format("woff")`,
+      `url("/fonts/Pretendard-Medium.woff2") format("woff2")`,
+      `local("Pretendard-Medium")`,
+    ],
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontDisplay: "swap",
+  },
+]);
+
+export const Rubik = fontFace([
+  {
+    src: [
+      `url("/fonts/Rubik-Regular.woff") format("woff")`,
+      `url("/fonts/Rubik-Regular.woff2") format("woff2")`,
+      `local("Rubik-Regular")`,
+    ],
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontDisplay: "swap",
+  },
+  {
+    src: [
+      `url("/fonts/Rubik-Medium.woff") format("woff")`,
+      `url("/fonts/Pretendard-Medium.woff2") format("woff2")`,
+      `local("Pretendard-Medium")`,
+    ],
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontDisplay: "swap",
+  },
+]);
