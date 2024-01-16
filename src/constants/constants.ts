9ae029617c2408cbe7ea9a328a2ccc0b0436a415
365support <365support@naver.com>
2024-01-16 15:03:45 +0900

    feat: Time type 지정, template -> Schedule[] 변환 함수 추가

---
+++ src/constants/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_TEMPLATE = {
+  work: { minutes: 1, seconds: 0 },
+  rest: { minutes: 1, seconds: 0 },
+  round: 2,
+  cycle: 2,
+  cycleRest: { minutes: 1, seconds: 0 },
+};
---
+++ src/utils/createScheduleFromTemplate.ts
@@ -0,0 +1,24 @@
+import { Schedule, Template } from "@/types/Time";
+
+const createRoundSchedules = (template: Template): Schedule[] => {
+  return Array.from({ length: template.round }, (_, index) => {
+    const isNotLastRound = index < template.round - 1;
+
+    return [
+      { name: "work", time: template.work },
+      ...(isNotLastRound ? [{ name: "rest", time: template.rest }] : []),
+    ];
+  }).flat();
+};
+
+export const createScheduleFromTemplate = (template: Template): Schedule[] => {
+  const roundSchedules = createRoundSchedules(template);
+
+  return Array.from({ length: template.cycle }, (_, cycleIndex) => {
+    const isNotLastCycle = cycleIndex < template.cycle - 1;
+
+    return isNotLastCycle
+      ? [...roundSchedules, { name: "cycleRest", time: template.cycleRest }]
+      : [...roundSchedules];
+  }).flat();
+};
