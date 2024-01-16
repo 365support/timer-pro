40ca710ef743f3bc0334401808c29bdcb9db8cc0
365support <365support@naver.com>
2024-01-16 15:06:29 +0900

    feat: Template 입력 기능 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "../styles/global.css";
-
-// const inter = Inter({ subsets: ["latin"] });
+import * as style from "./layout.css";
 
 export const metadata: Metadata = {
   manifest: "/manifest.json",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={style.body}>{children}</body>
     </html>
   );
 }
