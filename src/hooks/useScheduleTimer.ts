9e2e871ae986aa3d5d029943ca909f6e423f3520
365support <365support@naver.com>
2024-01-30 00:54:44 +0900

    feat: 전체 남은시간, 타이머 중지시 시간 보존, 다음 스케줄 타이머 실행 할 수 있도록 구현

--- src/hooks/useScheduleTimer.ts
+++ src/hooks/useScheduleTimer.ts
@@ -1,42 +1,101 @@
 "use client";
 import { useState } from "react";
-import { Schedule } from "@/types/Time";
+import { Schedule, Time } from "@/types/Time";
 import useTimer from "./useTimer";
+import { isTimeNonZero } from "@/utils/isTimeNonZero";
+import { selectAppropriateTime } from "@/utils/selectAppropriateTime";
+import { checkTimerStatus } from "@/utils/checkTimerStatus";
+import { adjustTimeToPositive } from "@/utils/adjustTimeToPositive";
 
 type ScheduleTimer = {
   schedules: Schedule[];
+  totalTime: Time;
   onCancel: () => void;
 };
 
-const useScheduleTimer = ({ schedules, onCancel }: ScheduleTimer) => {
-  const [currentSchedulesIndex, setCurrentSchedulesIndex] = useState(0);
-  const [scheduleTimerIsRunning, setScheduleTimerIsRunning] = useState(false);
-
+const useScheduleTimer = ({
+  schedules,
+  totalTime,
+  onCancel,
+}: ScheduleTimer) => {
   const { currentTime, isRunning, startTimer, stopTimer, resetTimer } =
     useTimer();
 
+  const {
+    currentTime: currentTotalTime,
+    startTimer: startTotalTimer,
+    stopTimer: stopTotalTimer,
+    resetTimer: resetTotalTimer,
+  } = useTimer();
+
+  const [currentSchedulesIndex, setCurrentSchedulesIndex] = useState(0);
+  const [totalTimerIsRunning, setTotalTimerIsRunning] = useState(false);
+
+  const [pausedTime, setPausedTime] = useState({ minutes: 0, seconds: 0 });
+  const [totalPausedTime, setTotalPausedTime] = useState({
+    minutes: 0,
+    seconds: 0,
+  });
+
   const startScheduleTimerForIndex = (index: number) => {
-    const { minutes, seconds } = schedules[index].time;
-    startTimer(minutes, seconds, nextScheduleTimer);
+    const isPausedTimeZero = isTimeNonZero(pausedTime);
+    const scheduleTime = selectAppropriateTime(
+      isPausedTimeZero,
+      pausedTime,
+      schedules[index].time
+    );
+
+    startTimer(scheduleTime.minutes, scheduleTime.seconds, nextScheduleTimer);
   };
 
   const startScheduleTimer = () => {
     const canStartTimer = schedules.length > 0 && !isRunning;
 
+    const isPausedTimeZero = isTimeNonZero(totalPausedTime);
+    const totalTimerTime = selectAppropriateTime(
+      isPausedTimeZero,
+      totalPausedTime,
+      totalTime
+    );
+
     if (canStartTimer) {
-      setScheduleTimerIsRunning(true);
+      setTotalTimerIsRunning(true);
       startScheduleTimerForIndex(currentSchedulesIndex);
+      startTotalTimer(totalTimerTime.minutes, totalTimerTime.seconds);
     }
   };
 
+  const updateTimerForNextSchedule = (
+    prevIndex: number,
+    updatedIndex: number
+  ) => {
+    const currentScheduleTime = schedules[prevIndex].time;
+    totalTime.minutes -= currentScheduleTime.minutes;
+    totalTime.seconds -= currentScheduleTime.seconds;
+
+    adjustTimeToPositive(totalTime);
+
+    const nextScheduleTime = schedules[updatedIndex].time;
+    startTimer(
+      nextScheduleTime.minutes,
+      nextScheduleTime.seconds,
+      nextScheduleTimer
+    );
+
+    startTotalTimer(totalTime.minutes, totalTime.seconds);
+  };
+
   const nextScheduleTimer = () => {
-    if (isRunning) return;
     setCurrentSchedulesIndex((prevIndex) => {
       const updatedIndex = prevIndex + 1;
-      if (updatedIndex < schedules.length) {
+      const hasNextSchedule = updatedIndex < schedules.length;
+      if (hasNextSchedule) {
         startScheduleTimerForIndex(updatedIndex);
+        setPausedTime({ minutes: 0, seconds: 0 });
+
+        updateTimerForNextSchedule(prevIndex, updatedIndex);
       } else {
-        setScheduleTimerIsRunning(false);
+        setTotalTimerIsRunning(false);
       }
       return updatedIndex;
     });
@@ -44,32 +103,60 @@ const useScheduleTimer = ({ schedules, onCancel }: ScheduleTimer) => {
 
   const stopScheduleTimer = () => {
     stopTimer();
-    setScheduleTimerIsRunning(false);
+    setPausedTime(currentTime);
+
+    stopTotalTimer();
+    setTotalPausedTime(currentTotalTime);
+
+    setTotalTimerIsRunning(false);
   };
 
   const cancelScheduleTimer = () => {
     resetTimer();
-    setScheduleTimerIsRunning(false);
+    resetTotalTimer();
+
+    setTotalTimerIsRunning(false);
+
     onCancel();
   };
 
+  const displayCurrentTime = () => {
+    const status = checkTimerStatus(totalTimerIsRunning, currentSchedulesIndex);
+    if (status === "notStarted") {
+      return schedules[0].time;
+    } else if (status === "running") {
+      return currentTime;
+    }
+    return pausedTime;
+  };
+
+  const displayCurrentTotalTime = () => {
+    const status = checkTimerStatus(totalTimerIsRunning, currentSchedulesIndex);
+    if (status === "notStarted") {
+      return totalTime;
+    } else if (status === "running") {
+      return currentTotalTime;
+    }
+    return totalPausedTime;
+  };
+
   const currentSchedule = schedules[currentSchedulesIndex];
-  const nextSchedule =
-    currentSchedulesIndex < schedules.length - 1
-      ? schedules[currentSchedulesIndex + 1]
-      : null;
+  const hasNextSchedule = currentSchedulesIndex < schedules.length - 1;
+  const nextSchedule = hasNextSchedule && schedules[currentSchedulesIndex + 1];
 
   return {
     currentSchedulesIndex,
     currentSchedule,
     nextSchedule,
-    currentTime,
+    currentTime: displayCurrentTime(),
     isRunning,
     startScheduleTimer,
     nextScheduleTimer,
     cancelScheduleTimer,
     stopScheduleTimer,
-    scheduleTimerIsRunning,
+    currentTotalTime: displayCurrentTotalTime(),
+
+    totalTimerIsRunning,
   };
 };
 
