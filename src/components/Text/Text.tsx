9fc4dae927f6917cc846c29dc8228b8cc434b9d7
365support <365support@naver.com>
2024-02-05 19:08:32 +0900

    style: text lineheight 수정

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -21,7 +21,7 @@ function Text<T extends ElementType = "span">(
   {
     as,
     color = "black",
-    lineHeight = "1.5",
+    lineHeight = "1",
     weight = "normal",
     size = "16px",
     align = "left",
