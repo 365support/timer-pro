e26796b74a5d31f68b81e2009e0e680aeea07c2f
365support <365support@naver.com>
2024-02-05 18:57:11 +0900

    fix: useLocalStorage hydration error 수정

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import { getLocalStorageItem, setLocalStorageItem } from "@/utils/localStorage";
 
+const isServer = typeof window === "undefined";
+
 function useLocalStorage<T>(
   key: string,
   defaultValue: T
@@ -9,23 +11,39 @@ function useLocalStorage<T>(
   key: string
 ): [T | null, Dispatch<SetStateAction<T | null>>];
 function useLocalStorage<T>(key: string, defaultValue?: T) {
-  const [value, setValue] = useState(() => {
-    if (typeof window === "undefined") {
-      return defaultValue ?? null;
-    }
-    const storedValue = getLocalStorageItem<T>(key);
-
-    if (!storedValue) return defaultValue ?? null;
-    return storedValue;
+  const [storedValue, setStoredValue] = useState<T | null>(() => {
+    return defaultValue ?? null;
   });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      setLocalStorageItem(key, value);
+    if (!isServer) {
+      try {
+        const item = getLocalStorageItem<T>(key);
+        const value = item ?? defaultValue ?? null;
+        setStoredValue(value);
+        setLocalStorageItem(key, value);
+      } catch (error) {
+        console.error(error);
+        setStoredValue(defaultValue ?? null);
+      }
     }
-  }, [key, value]);
+  }, [key, defaultValue]);
 
-  return [value, setValue];
+  const setValue: Dispatch<SetStateAction<T | null>> = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+
+      if (!isServer) {
+        setLocalStorageItem(key, valueToStore);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default useLocalStorage;
