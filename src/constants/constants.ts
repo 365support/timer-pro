471b00b0e09056cf2138a2666b0d388d9d835c7b
365support <365support@naver.com>
2024-02-12 21:04:30 +0900

    feat: SCHEDULE_NAME 타입 정의 및 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,5 @@
 import { Preset, Round, Setting, StopWatch } from "@/components/Common/Icons";
+import { TabataTemplate } from "@/types/Time";
 
 export const DEFAULT_TEMPLATE = {
   work: { minutes: 0, seconds: 30 },
@@ -17,3 +18,13 @@ export const NAV_ITEMS_SECOND_GROUP = [
   { route: "/preset", icon: Preset, label: "프리셋" },
   { route: "/setting", icon: Setting, label: "설정" },
 ];
+
+export type ScheduleNameType = Record<keyof TabataTemplate, string>;
+
+export const SCHEDULE_NAME: ScheduleNameType = {
+  work: "운동",
+  rest: "휴식",
+  cycleRest: "세트 휴식",
+  round: "라운드",
+  cycle: "사이클",
+};
