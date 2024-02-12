639b7cd7a325dbdb200ef21c1d04284f5eea6b4d
365support <365support@naver.com>
2024-02-12 21:13:03 +0900

    feat: ScheduleTimer 페이지 css, progressbar 적용

--- src/components/ScheduleTimer/index.tsx
+++ src/components/ScheduleTimer/index.tsx
@@ -2,18 +2,24 @@
 import { useRouter } from "next/navigation";
 import useScheduleTimer from "@/hooks/useScheduleTimer";
 import { useScheduleTemplate } from "@/hooks/useScheduleTemplate";
+import { SCHEDULE_NAME } from "@/constants/constants";
 import { Schedule, TemplateType } from "@/types/Time";
+import calculateProgressPercent from "@/utils/calculateProgressPercent";
 import CurrentSchedule from "./Component/CurrentSchedule";
 import NextSchedule from "./Component/NextSchedule";
 import ScheduleCounter from "./Component/ScheduleCounter";
 import ControlPanel from "./Component/ControlPanel";
 import RestTime from "./Component/RestTime";
+import TimerHeader from "./Component/TimerHeader";
+import Progressbar from "./Component/Progressbar";
+import * as styles from "./index.css";
 
 interface Props {
   schedules: Schedule[];
   TemplateType: TemplateType;
 }
 
+// TODO: 운동 끝났을 때 모달창 띄우고 메인페이지 이동
 const ScheduleTimer = ({ schedules, TemplateType }: Props) => {
   const { cycle, totalWork } = useScheduleTemplate(TemplateType);
 
@@ -38,25 +44,51 @@ const ScheduleTimer = ({ schedules, TemplateType }: Props) => {
   });
 
   return (
-    <div>
-      <ScheduleCounter
-        currentSchedule={currentSchedule}
-        totalWork={totalWork}
-        cycle={cycle}
+    <div className={styles.mainContainer}>
+      <TimerHeader
+        onClickLeft={navigateToMainPage}
+        isLock={false}
+        scheduleName={SCHEDULE_NAME[currentSchedule.name]}
       />
-      {currentSchedule && (
-        <CurrentSchedule name={currentSchedule.name} time={currentTime} />
-      )}
-      <RestTime time={currentTotalTime} />
-      {nextSchedule && <NextSchedule name={nextSchedule.name} />}
-      <ControlPanel
-        isRunning={totalTimerIsRunning}
-        onStart={startScheduleTimer}
-        onStop={stopScheduleTimer}
-        onCancel={cancelScheduleTimer}
-        onNext={nextScheduleTimer}
-        hasNext={!!nextSchedule}
-      />
+
+      <div className={styles.innerContainer}>
+        <ScheduleCounter
+          currentSchedule={currentSchedule}
+          totalWork={totalWork}
+          cycle={cycle}
+        />
+
+        <div className={styles.progressStyle}>
+          <Progressbar
+            percent={calculateProgressPercent({
+              totalTime: currentSchedule.time,
+              elapsedTime: currentTime,
+            })}
+          >
+            <div className={styles.displayFlex}>
+              {currentSchedule && (
+                <CurrentSchedule
+                  name={SCHEDULE_NAME[currentSchedule.name]}
+                  time={currentTime}
+                />
+              )}
+              <RestTime time={currentTotalTime} />
+            </div>
+          </Progressbar>
+          {nextSchedule && (
+            <NextSchedule name={SCHEDULE_NAME[nextSchedule.name]} />
+          )}
+        </div>
+
+        <ControlPanel
+          isRunning={totalTimerIsRunning}
+          onStart={startScheduleTimer}
+          onStop={stopScheduleTimer}
+          onCancel={cancelScheduleTimer}
+          onNext={nextScheduleTimer}
+          hasNext={!!nextSchedule}
+        />
+      </div>
     </div>
   );
 };
