671999f28bca85f2ff720b521b08fbe43b99a1b0
365support <365support@naver.com>
2024-02-12 21:11:51 +0900

    feat: breakpoint 상수 추가

---
+++ src/styles/breakPoint.ts
@@ -0,0 +1,5 @@
+export const breakPoint = {
+  smallPhone: { width: "360px", height: "660px" },
+  bigPhone: { width: "480px", height: "800px" },
+  tablet: { width: "768px", height: "1024px" },
+};
